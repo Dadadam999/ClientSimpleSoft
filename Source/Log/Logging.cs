fd7c9a13ecb05ff503c48c0d259ecd0b5a5799cc
Dadadam <bogdanov.andrey.code@gmail.com>
2024-03-17 01:33:06 +0200

    Add isEnabled flag, check formIdField and remake logs

---
+++ Source/Log/Logging.cs
@@ -0,0 +1,20 @@
+﻿namespace ClientSimpleSoft
+{
+    public static class Logging
+    {
+        private static string filePath = "lastlog.txt";
+
+        public static void Create( Log log )
+        {
+            File.AppendAllText( filePath, string.Join( " ", log.List ) );
+        }
+
+        public static void DeleteFile()
+        {
+            if( File.Exists( filePath ) )
+            {
+                File.Delete( filePath );
+            }
+        }
+    }
+}
